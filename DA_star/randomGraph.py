8d702b85127780fbce709192a02b72cfaa1c4846
lkoshale <lkoshale@gmail.com>
2019-09-21 01:42:14 +0530

    0 weight edge removed

--- DA_star/randomGraph.py
+++ DA_star/randomGraph.py
@@ -9,5 +9,5 @@ file = open("pygraph","w")
 for i in range(0,E):
     a = random.randint(0,N-1)
     b = random.randint(0,N-1)
-    c = random.randint(0,100)
+    c = random.randint(10,100)
     file.write("{} {} {}\n".format(a,b,c))
